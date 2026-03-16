ebb3adb773af10b48882216464edb6c80784d6c1
Alex McKinney <alexmckinney01@gmail.com>
2026-03-16 16:29:45 -0400

    feat(openapi): Remove go_package option requirement

--- cmd/protoc-gen-openapi/main.go
+++ cmd/protoc-gen-openapi/main.go
@@ -17,11 +17,16 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
+	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/fern-api/protoc-gen-openapi/cmd/protoc-gen-openapi/generator"
 	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/descriptorpb"
 	"google.golang.org/protobuf/types/pluginpb"
 )
 
@@ -46,25 +51,77 @@ func main() {
 		ParamFunc: flags.Set,
 	}
 
-	opts.Run(func(plugin *protogen.Plugin) error {
-		// Enable "optional" keyword in front of type (e.g. optional string label = 1;)
-		plugin.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
-		if *conf.OutputMode == "source_relative" {
-			for _, file := range plugin.Files {
-				if !file.Generate {
-					continue
-				}
-				outfileName := strings.TrimSuffix(file.Desc.Path(), filepath.Ext(file.Desc.Path())) + ".openapi.yaml"
-				outputFile := plugin.NewGeneratedFile(outfileName, "")
-				gen := generator.NewOpenAPIv3Generator(plugin, conf, []*protogen.File{file})
-				if err := gen.Run(outputFile); err != nil {
-					return err
-				}
+	if err := run(opts, conf); err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", filepath.Base(os.Args[0]), err)
+		os.Exit(1)
+	}
+}
+
+func run(opts protogen.Options, conf generator.Configuration) error {
+	if len(os.Args) > 1 {
+		return fmt.Errorf("unknown argument %q (this program should be run by protoc, not directly)", os.Args[1])
+	}
+	in, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		return err
+	}
+	req := &pluginpb.CodeGeneratorRequest{}
+	if err := proto.Unmarshal(in, req); err != nil {
+		return err
+	}
+	// The protogen library requires every .proto file to have a go_package option,
+	// but this plugin generates OpenAPI specs — not Go code. Inject a synthetic
+	// go_package for any file that doesn't already have one so users aren't forced
+	// to add a Go-specific option to their proto files.
+	for _, fdesc := range req.ProtoFile {
+		if fdesc.GetOptions().GetGoPackage() == "" {
+			if fdesc.Options == nil {
+				fdesc.Options = &descriptorpb.FileOptions{}
 			}
-		} else {
-			outputFile := plugin.NewGeneratedFile("openapi.yaml", "")
-			return generator.NewOpenAPIv3Generator(plugin, conf, plugin.Files).Run(outputFile)
+			pkg := fdesc.GetPackage()
+			if pkg == "" {
+				pkg = strings.TrimSuffix(filepath.Base(fdesc.GetName()), ".proto")
+			}
+			goPackage := "example.com/fake/" + strings.ReplaceAll(pkg, ".", "/")
+			fdesc.Options.GoPackage = &goPackage
 		}
-		return nil
-	})
+	}
+	gen, err := opts.New(req)
+	if err != nil {
+		return err
+	}
+	if err := generate(gen, conf); err != nil {
+		gen.Error(err)
+	}
+	resp := gen.Response()
+	out, err := proto.Marshal(resp)
+	if err != nil {
+		return err
+	}
+	if _, err := os.Stdout.Write(out); err != nil {
+		return err
+	}
+	return nil
+}
+
+func generate(plugin *protogen.Plugin, conf generator.Configuration) error {
+	// Enable "optional" keyword in front of type (e.g. optional string label = 1;)
+	plugin.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+	if *conf.OutputMode == "source_relative" {
+		for _, file := range plugin.Files {
+			if !file.Generate {
+				continue
+			}
+			outfileName := strings.TrimSuffix(file.Desc.Path(), filepath.Ext(file.Desc.Path())) + ".openapi.yaml"
+			outputFile := plugin.NewGeneratedFile(outfileName, "")
+			gen := generator.NewOpenAPIv3Generator(plugin, conf, []*protogen.File{file})
+			if err := gen.Run(outputFile); err != nil {
+				return err
+			}
+		}
+	} else {
+		outputFile := plugin.NewGeneratedFile("openapi.yaml", "")
+		return generator.NewOpenAPIv3Generator(plugin, conf, plugin.Files).Run(outputFile)
+	}
+	return nil
 }
